3edd9f65600652dbc967f778c479fd9da5cc41f3
Dave <dlp140@gmail.com>
2022-05-19 16:45:43 -0400

    changed index from unordered list to images that link to show pages

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const port = 3000;
 // Mount Middleware
 
 // Index GET/pokemon
+app.get("/", (req, res) => {
+  res.render("index.ejs", { pokemon: pokemon });
+});
 
 // New GET/pokemon/new
 
@@ -24,7 +27,11 @@ const port = 3000;
 // Edit GET/pokemon
 
 // Show GET/pokemon/:id
-
+app.get("/pokemon/:indexOfPokemon", (req, res) => {
+  res.render("show.ejs", {
+    pokemon: pokemon[req.params.indexOfPokemon],
+  });
+});
 // Port listening
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
