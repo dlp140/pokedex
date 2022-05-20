2db6323a92230780d93c6b8d8081787cde5992fa
Dave <dlp140@gmail.com>
2022-05-19 22:35:04 -0400

    delete buttons working on index and show pages. edit button leads to edit page, submit button does not update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Require Dependencies
 const express = require("express");
+const methodOverride = require("method-override");
 
 const pokemon = require("./models/pokemon");
 
@@ -10,30 +11,43 @@ const app = express();
 const port = 3000;
 
 // Mount Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 // Index GET/pokemon
-app.get("/", (req, res) => {
+app.get("/pokemon/", (req, res) => {
   res.render("index.ejs", { pokemon: pokemon });
 });
 
 // New GET/pokemon/new
 
 // Destroy DELETE/pokemon:id
-app.delete("/pokemon/:indexOfPokemon", (req, res) => {
-  pokemon.splice(req.params.indexOfPokemon, 1);
-  res.redirect("/pokemon");
+app.delete("/pokemon/:id", (req, res) => {
+  pokemon.splice(req.params.id, 1);
+  res.redirect("/pokemon/");
 });
 
 // Update PUT/pokemon/:id
+app.put("/pokemon/:id", (req, res) => {
+  pokemon[req.params.id] = req.body;
+  res.redirect("/pokemon");
+});
 
 // Create POST/pokemon
 
 // Edit GET/pokemon
+app.get("/pokemon/:id/edit", (req, res) => {
+  res.render("edit.ejs", {
+    pokemon: pokemon[req.params.id],
+    index: req.params.id,
+  });
+});
 
 // Show GET/pokemon/:id
-app.get("/pokemon/:indexOfPokemon", (req, res) => {
+app.get("/pokemon/:id", (req, res) => {
   res.render("show.ejs", {
-    pokemon: pokemon[req.params.indexOfPokemon],
+    pokemon: pokemon[req.params.id],
+    index: req.params.id,
   });
 });
 
