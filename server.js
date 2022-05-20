2a08d17a1d5904334adc925e86812e4321c5e72c
Dave <dlp140@gmail.com>
2022-05-20 15:49:06 -0400

    new pokemon can be added, types not working

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Require Dependencies
 const express = require("express");
+const { type } = require("express/lib/response");
 const methodOverride = require("method-override");
 const { pop } = require("./models/pokemon");
 
@@ -33,20 +34,41 @@ app.delete("/pokemon/:id", (req, res) => {
 
 // Update PUT/pokemon/:id
 app.put("/pokemon/:id", (req, res) => {
+  pokemon[req.params.id] = req.body;
   res.redirect("/pokemon");
   console.log(req.body);
 });
 
 // Create POST/pokemon
 app.post("/pokemon", (req, res) => {
-  pokemon.push(req.body);
+  newPokemon = {
+    name: req.body.name,
+    id: req.body.id,
+    img: "https://i.imgur.com/Rwjez9Q.jpg",
+    type: req.body.type,
+    misc: {
+      height: req.body.height,
+      weight: req.body.weight,
+    },
+    stats: {
+      hp: req.body.hp,
+      attack: req.body.attack,
+      defense: req.body.defense,
+      spattack: req.body.spattack,
+      spdefense: req.body.spdefense,
+      speed: req.body.speed,
+    },
+  };
+  pokemon.push(newPokemon);
   res.redirect("/pokemon");
+  console.log(newPokemon);
 });
 
 // Edit GET/pokemon
-app.get("/pokemon/:id/edit", (req, res) => {
+app.get("/pokemon/:id/edit/", (req, res) => {
   res.render("edit.ejs", {
     pokemon: pokemon[req.params.id],
+    hp: pokemon[req.params.id].stats.hp,
     index: req.params.id,
   });
 });
